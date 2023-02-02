eff8e1f402ea0306ba18ca7256d94970c32f4d2f
douglas santanna <douglbb1@gmail.com>
2023-02-02 15:00:23 -0300

    first commit

---
+++ message-receiver/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.ServiceBus;
+
+public class Receiver //Consumidor
+{
+    static string connString = "Endpoint=sb://sb-teste-ivo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=GQjH8vFzPYC03olK2veqvPOEjNoB7iSsnOcyuGuZkwU=";
+    static string topicName = "norgelabs";
+    static string fila = "reembolso";
+    static async Task Main(string[] args)
+    {
+        var client = new ServiceBusClient(connString);
+        var sender = client.CreateReceiver(topicName, fila);
+        Console.WriteLine("Processando mensagens...");
+        while (true)
+        {
+            var message = await sender.ReceiveMessageAsync();
+            if (message is not null)
+            {
+                System.Console.WriteLine($"Processando: {message.Body}");
+                await sender.CompleteMessageAsync(message);
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Todas os pagamentos foram processados");
+                break;
+            }
+        }
+        await sender.CloseAsync();
+    }
+}
---
+++ message-sender/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+using Azure.Messaging.ServiceBus;
+
+public class Sender //Publicador
+{
+    static string connString = "Endpoint=sb://sb-teste-ivo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=GQjH8vFzPYC03olK2veqvPOEjNoB7iSsnOcyuGuZkwU=";
+    static string topicName = "norgelabs";
+    static async Task Main(string[] args)
+    {
+        List<TipoPagamento> pagamentos = new List<TipoPagamento>();
+        pagamentos.Add(new TipoPagamento() { Id = 1, tipoPagamento = "reembolso" });
+        pagamentos.Add(new TipoPagamento() { Id = 2, tipoPagamento = "dinheiro" });
+        pagamentos.Add(new TipoPagamento() { Id = 3, tipoPagamento = "boleto" });
+        pagamentos.Add(new TipoPagamento() { Id = 4, tipoPagamento = "reembolso" });
+        pagamentos.Add(new TipoPagamento() { Id = 5, tipoPagamento = "pix" });
+
+        var client = new ServiceBusClient(connString);
+        var sender = client.CreateSender(topicName);
+        Console.WriteLine("Enviando mensagens...");
+        foreach (var pagamento in pagamentos)
+        {
+            var message = new ServiceBusMessage(JsonSerializer.Serialize(pagamento));
+            message.ApplicationProperties.Add("tipoPagamento", pagamento.tipoPagamento.ToString());
+            System.Console.WriteLine($"Enviando: {pagamento.tipoPagamento}");
+            await sender.SendMessageAsync(message);
+        }
+        await sender.CloseAsync();
+        System.Console.WriteLine("Mensagens enviadas");
+    }
+}
+public class TipoPagamento
+{
+    public int Id { get; set; }
+    public string tipoPagamento { get; set; } = string.Empty;
+
+}
